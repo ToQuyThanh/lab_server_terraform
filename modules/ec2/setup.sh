95451751a5e971e023396c06f7a5a8098e53721e
thanh.toquy <thanh.toquy@vti.com.vn>
2024-11-27 14:09:40 +0700

    Update install docker script

--- modules/ec2/setup.sh
+++ modules/ec2/setup.sh
@@ -5,13 +5,12 @@ sudo amazon-linux-extras enable docker
 sudo yum install -y docker
 
 # Start Docker service
-sudo service docker start
+sudo systemctl start docker
+sudo systemctl enable docker
 
 # Add ec2-user to Docker group and apply the new group settings
-sudo usermod -a -G docker $USER
-sudo systemctl enable docker
+sudo usermod -a -G docker ec2-user
 
 # Install docker-compose
 sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-
